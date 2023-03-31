ceed645c4fc0ffb6580bbf559327e594db6bfe1b
ryshen <ryshen98@outlook.com>
2023-03-31 16:05:48 -0500

    Añadido vista

--- Problema 1/ejercicio.js
+++ Problema 1/ejercicio.js
@@ -1,1 +1,12 @@
-console.log("Starting");
+let figura = prompt('Ingrese la figura a la cual desea encontrar su area o perimetro (1.tringulo, 2.rectangulo, 3.cuadrado, 4.circulo)');
+switch (figura) {
+  case "1":
+    console.log("hola");
+    break;
+  case "2":
+    break;
+  case "3":
+    break;
+  case "4":
+    break;
+}
